ce894d0990dd21dd941d2dac3208daffaf002b63
TosyaReva <tosyan93@gmail.com>
2025-02-09 02:11:46 +0200

    Deleted extra file

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,13 +2,12 @@ import css from "./FriendListItem.module.css";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   // isOnline color css
+  const statusClassName = isOnline ? css.online : css.offline;
   return (
     <div className={css.card}>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p className={isOnline ? css.online : css.offline}>
-        {isOnline ? "Online" : "Offline"}
-      </p>
+      <p className={statusClassName}>{isOnline ? "Online" : "Offline"}</p>
     </div>
   );
 }
