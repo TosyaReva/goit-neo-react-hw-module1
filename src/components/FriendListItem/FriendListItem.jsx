7101ac27a90a979f90a6d83332e1121deab87fd7
TosyaReva <tosyan93@gmail.com>
2025-02-09 02:03:39 +0200

    Task completed

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,14 @@
+import css from "./FriendListItem.module.css";
+
+export default function FriendListItem({ avatar, name, isOnline }) {
+  // isOnline color css
+  return (
+    <div className={css.card}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p className={isOnline ? css.online : css.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+}
